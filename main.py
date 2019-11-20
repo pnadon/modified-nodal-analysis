911ef0efea0147eae75cc843045e62d443271fae
Phil Nadon <phil@nadon.io>
2019-11-20 09:59:02 -0700

    new stats

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from os import listdir
 from lib import gen_rand_circuit_file
 from mna_solver import benchmark_MNA
 
-# for i in range(3, 64):
-#     gen_rand_circuit_file("rand_data/rand_{:03}.csv".format(i), int(sqrt(2 ** i)), 3)
+for i in range(3, 25):
+    gen_rand_circuit_file("rand_data/rand_{:03}.csv".format(i), int(sqrt(2 ** i)), 3)
 
 with open("stats.csv", "w+") as stats_file:
     print(
-        "fname,matrix_size,num_components,cpu_total,cpu_transfer,cpu_solve,cuda_total,cuda_transfer,cuda_solve",
+        "fname,matrix_size,num_components,processor,total,transfer,solve",
         file=stats_file,
     )
 for fname in listdir("rand_data"):
@@ -17,16 +17,26 @@ for fname in listdir("rand_data"):
     benchmark = benchmark_MNA(df)
     with open("stats.csv", "a+") as stats_file:
         print(
-            "{},{},{},{},{},{},{},{},{}".format(
+            "{},{},{},{},{},{},{}".format(
                 fname,
                 benchmark["matrix_size"][0],
                 benchmark["num_components"],
-                benchmark["cpu"]["total"],
-                benchmark["cpu"]["data transfer"],
-                benchmark["cpu"]["linear solve"],
+                'cuda',
                 benchmark["cuda"]["total"],
                 benchmark["cuda"]["data transfer"],
                 benchmark["cuda"]["linear solve"],
+            ),
+            file=stats_file,
+        )
+        print(
+            "{},{},{},{},{},{},{}".format(
+                fname,
+                benchmark["matrix_size"][0],
+                benchmark["num_components"],
+                'cpu',
+                benchmark["cpu"]["total"],
+                benchmark["cpu"]["data transfer"],
+                benchmark["cpu"]["linear solve"],
             ),
             file=stats_file,
         )
