3884577ae983e961435ec1d2a2b0bf6e4c003358
Phil Nadon <phil@nadon.io>
2019-11-20 12:46:07 -0700

    update to include more info in CSV

--- main.py
+++ main.py
@@ -6,22 +6,24 @@ from mna_solver import benchmark_MNA
 
 with open("stats.csv", "w+") as stats_file:
     print(
-        "fname,matrix_size,density,num_components,processor,total,transfer,solve",
+        "fname,matrix_size,num_nodes,density,num_components,processor,total,transfer,solve",
         file=stats_file,
     )
 
 for i in range(2, 11, 2):
     for j in range(3, 25):
         fname = "rand_data/rand_size-{:03}_density-{:03}.csv".format(j,i)
-        gen_rand_circuit_file(fname, int(sqrt(2 ** j)), i)
+        num_nodes = int(sqrt(2 ** j))
+        gen_rand_circuit_file(fname, num_nodes, i)
         df = pd.read_csv(fname)
         benchmark = benchmark_MNA(df)
         with open("stats.csv", "a+") as stats_file:
             print(
-                "{},{},{},{},{},{},{}".format(
+                "{},{},{},{},{},{},{},{}".format(
                     fname,
                     benchmark["matrix_size"][0],
-                    i,
+                    j,
+                    num_nodes,
                     benchmark["num_components"],
                     'cuda',
                     benchmark["cuda"]["total"],
@@ -31,10 +33,11 @@ for i in range(2, 11, 2):
                 file=stats_file,
             )
             print(
-                "{},{},{},{},{},{},{}".format(
+                "{},{},{},{},{},{},{},{}".format(
                     fname,
                     benchmark["matrix_size"][0],
-                    i,
+                    j,
+                    num_nodes,
                     benchmark["num_components"],
                     'cpu',
                     benchmark["cpu"]["total"],
