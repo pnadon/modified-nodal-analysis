0199d825cf15c345420577a6a6bdb32f94b0181f
Phil Nadon <phil@nadon.io>
2019-11-20 12:23:30 -0700

    fixed issue in csv updater

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ for i in range(2, 11, 2):
                 "{},{},{},{},{},{},{}".format(
                     fname,
                     benchmark["matrix_size"][0],
+                    i
                     benchmark["num_components"],
                     'cuda',
                     benchmark["cuda"]["total"],
@@ -33,6 +34,7 @@ for i in range(2, 11, 2):
                 "{},{},{},{},{},{},{}".format(
                     fname,
                     benchmark["matrix_size"][0],
+                    i
                     benchmark["num_components"],
                     'cpu',
                     benchmark["cpu"]["total"],
